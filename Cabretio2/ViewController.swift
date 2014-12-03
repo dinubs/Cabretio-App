6a1d30287c30c3c730c59f34edc180b451beed2c
Gavin <gavin_dinubilo@mail.tmcc.edu>
2014-12-03 08:14:02 -0800

    Initial Commit

---
+++ Cabretio2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cabretio2
+//
+//  Created by Gavin Dinubilo on 12/3/14.
+//  Copyright (c) 2014 Gavin Dinubilo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Cabretio2Tests/Cabretio2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Cabretio2Tests.swift
+//  Cabretio2Tests
+//
+//  Created by Gavin Dinubilo on 12/3/14.
+//  Copyright (c) 2014 Gavin Dinubilo. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Cabretio2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
